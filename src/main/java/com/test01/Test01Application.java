bbcfe94055f5be3bdd359d3bc7f6bbd7e17f5488
zhuang.zhang@wuxcl.com <zhuang.zhang@wuxcl.com>
2019-07-31 14:30:36 +0800

    idea整合GitHub测试

---
+++ src/main/java/com/test01/Test01Application.java
@@ -0,0 +1,15 @@
+package com.test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("test01...........000");
+        SpringApplication.run(Test01Application.class, args);
+    }
+
+}
